e3a0885db088d2a8a4e2dcce49ed03137f423404
Chase Young <chasekyoung@gmail.com>
2022-03-31 15:00:33 -0400

    debugging

---
+++ seeds/index.js
@@ -0,0 +1,18 @@
+const sequelize = require('../config/connection');
+const seedComments = require('./comments');
+const seedPosts = require('./posts');
+const seedUsers = require('./users');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await seedUsers();
+
+  await seedPosts();
+
+  await seedComments();
+
+  process.exit(0);
+};
+
+seedAll();
